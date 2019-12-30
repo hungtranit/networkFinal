11271458de75fc6806ff103dc259b449ed6e03ac
Trần Phi Hùng <hungtranphi97@gmail.com>
2019-12-30 21:42:22 +0700

    done send recieve file

--- src/networkFinal/tcpsendRecieveFile/FileClient.java
+++ src/networkFinal/tcpsendRecieveFile/FileClient.java
@@ -5,10 +5,14 @@ import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
 
 public class FileClient {
 
@@ -60,5 +64,87 @@ public class FileClient {
 		} catch (Exception e) {
 
 		}
+	}
+
+	private void analysis(String line) throws IOException {
+		StringTokenizer st = new StringTokenizer(line);
+		String command = st.nextToken();
+		String sf, df;
+		switch (command) {
+		case "SET_SERVER_DIR":
+			netOut.writeUTF(line);
+			break;
+		case "SET_CLIENT_DIR":
+			try {
+				clientDir = st.nextToken();
+			} catch (Exception e) {
+				throw new NoSuchElementException("Bạn đã nhập thiếu đường dẫn!");
+			}
+			break;
+		case "SEND":
+			try {
+				sf = st.nextToken();
+				df = st.nextToken();
+			} catch (Exception e) {
+				throw new NoSuchElementException("Nhập thiếu kìa bạn gì gì ơi!");
+			}
+			netOut.writeUTF(command + " " + df);
+			sendFile(sf);
+			break;
+		case "GET":
+			try {
+				sf = st.nextToken();
+				df = st.nextToken();
+			} catch (Exception e) {
+				throw new NoSuchElementException("Nhập thiếu kìa bạn gì gì ơi!");
+			}
+			netOut.writeUTF(command + " " + sf);
+			netOut.flush();
+			recivieFile(df);
+			break;
+		case "QUIT":
+			netOut.writeUTF(line);
+			break;
+
+		default:
+			break;
+		}
+		netOut.flush();
+	}
+
+	private void recivieFile(String df) throws IOException {
+		File file = new File(clientDir + File.separator + df);
+		BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file));
+		long size = netIn.readLong();
+		int byteRead, byteMustRead;
+		long remain = size;
+
+		byte[] buff = new byte[10 * 1024];
+
+		while (remain > 0) {
+			byteMustRead = buff.length > remain ? (int) remain : buff.length;
+			byteRead = netIn.read(buff, 0, byteMustRead);
+			out.write(buff);
+			remain -= byteRead;
+		}
+		out.close();
+	}
+
+	private void sendFile(String sf) throws IOException {
+		File file = new File(sf);
+		BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+		netOut.writeLong(file.length());
+		byte[] buff = new byte[10 * 1024];
+		int data;
+		while ((data = in.read(buff)) != -1) {
+			netOut.write(buff, 0, data);
+		}
+		netOut.flush();
+		in.close();
+	}
+
+	public static void main(String[] args) {
+		FileClient fc = new FileClient();
+		fc.request();
 	}
 }
--- src/networkFinal/tcpsendRecieveFile/FileServerProcessing.java
+++ src/networkFinal/tcpsendRecieveFile/FileServerProcessing.java
@@ -1,5 +1,104 @@
 package networkFinal.tcpsendRecieveFile;
 
-public class FileServerProcessing {
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.StringTokenizer;
+
+public class FileServerProcessing extends Thread {
+
+	Socket socket;
+	DataInputStream netIn;
+	DataOutputStream netOut;
+	String serverDir;
+
+	public FileServerProcessing(Socket socket) {
+		try {
+			this.socket = socket;
+			netIn = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+			netOut = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	@Override
+	public void run() {
+		try {
+			String line;
+			while (true) {
+				line = netIn.readUTF();
+				if (line.equalsIgnoreCase("QUIT"))
+					break;
+				analysis(line);
+			}
+			netIn.close();
+			netOut.flush();
+			netOut.close();
+			socket.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	private void analysis(String line) throws IOException {
+		StringTokenizer st = new StringTokenizer(line);
+		String sf, df;
+		String command = st.nextToken();
+		switch (command) {
+		case "SET_SERVER_DIR":
+			serverDir = st.nextToken();
+			break;
+		case "SEND":
+			df = st.nextToken();
+			recivieFile(df);
+			break;
+		case "GET":
+			sf = st.nextToken();
+			sendFile(sf);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	private void recivieFile(String df) throws IOException {
+		File file = new File(serverDir + File.separator + df);
+		BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file));
+		long size = netIn.readLong();
+		int byteRead, byteMustRead;
+		long remain = size;
+
+		byte[] buff = new byte[10 * 1024];
+
+		while (remain > 0) {
+			byteMustRead = buff.length > remain ? (int) remain : buff.length;
+			byteRead = netIn.read(buff, 0, byteMustRead);
+			out.write(buff);
+			remain -= byteRead;
+		}
+		out.close();
+	}
+
+	private void sendFile(String sf) throws IOException {
+		File file = new File(sf);
+		BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+		netOut.writeLong(file.length());
+		byte[] buff = new byte[10 * 1024];
+		int data;
+		while ((data = in.read(buff)) != -1) {
+			netOut.write(buff, 0, data);
+		}
+		netOut.flush();
+		in.close();
+	}
 
 }
